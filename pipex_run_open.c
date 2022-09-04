f957edaaa2d8b6e878df13bd4d08c0e20c0ffeb8
hioikawa <101631407+uminomae@users.noreply.github.com>
2022-09-04 09:31:32 +0900

    oep()のfdをfile_fdにして詳細に

--- pipex_run_open.c
+++ pipex_run_open.c
@@ -6,7 +6,7 @@
 /*   By: hioikawa <hioikawa@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 00:51:31 by hioikawa          #+#    #+#             */
-/*   Updated: 2022/09/03 23:59:54 by hioikawa         ###   ########.fr       */
+/*   Updated: 2022/09/04 09:30:48 by hioikawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,14 @@
 static void	open_read_file(int *fd, char *argv);
 static void	open_write_file(int *fd, char *argv);
 
-//file[WRITE] is like a chmod 644
-	//TODO const
-	//char	*const	*argv = (char *const *)pipex->argv;
-	//int		*const	fd = pipex->file_fd;
 void	open_files_on_purpose(\
-				t_v_argv *v_argv, char *const *argv, int *fd, int in_out)
+				t_v_argv *v_argv, char *const *argv, int *file_fd, int read_or_write)
 {
-	if (in_out == READ)
-		open_read_file(fd, argv[1]);
-	else if (in_out == WRITE)
-		open_write_file(fd, argv[4]);
-	if (fd[READ] == -1 | fd[WRITE] == -1)
+	if (read_or_write == READ)
+		open_read_file(file_fd, argv[1]);
+	else if (read_or_write == WRITE)
+		open_write_file(file_fd, argv[4]);
+	if (file_fd[READ] == -1 | file_fd[WRITE] == -1)
 		exit_with_error(v_argv, "open()");
 }
 
@@ -35,6 +31,7 @@ static void	open_read_file(int *fd, char *argv)
 	fd[READ] = open(argv, O_RDONLY | O_CLOEXEC, 0);
 }
 
+//file[WRITE] is like a chmod 644 
 static void	open_write_file(int *fd, char *argv)
 {
 	fd[WRITE] = open(argv, O_WRONLY | O_CLOEXEC | O_CREAT | O_TRUNC, \
