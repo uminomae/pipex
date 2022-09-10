545a03014a852bb650d53632df2649cd833201c6
hioikawa <101631407+uminomae@users.noreply.github.com>
2022-09-10 23:57:05 +0900

    微修正

--- pipex_begin.c
+++ pipex_begin.c
@@ -6,7 +6,7 @@
 /*   By: hioikawa <hioikawa@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 00:49:55 by hioikawa          #+#    #+#             */
-/*   Updated: 2022/09/10 17:01:19 by hioikawa         ###   ########.fr       */
+/*   Updated: 2022/09/10 23:56:51 by hioikawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,19 @@ void	begin_pipex(t_pipex *pipex, int argc, char **argv)
 	set_arg_position(pipex, argv);
 	validate_argc(pipex, argc);
 	init_pid_list(pipex, argc, pipex->other_cmd);
+}
+
+static void	init_struct(t_pipex *pipex, char **argv)
+{
+	pipex->argv = argv;
+	pipex->pipe_list.head = NULL;
+	pipex->pipe_list.tail = NULL;
+	pipex->pid_list.head = NULL;
+	pipex->pid_list.tail = NULL;
+	pipex->v_argv.list_absolute_path_of_command = NULL;
+	pipex->v_argv.list_of_directory = NULL;
+	pipex->v_argv.temp_divided_list = NULL;
+	pipex->v_argv.virtual_argv = NULL;
 }
 
 static void	set_arg_position(t_pipex *pipex, char **argv)
@@ -45,14 +58,6 @@ static void	set_arg_position(t_pipex *pipex, char **argv)
 	}
 }
 
-static bool	is_here_doc(char **argv)
-{
-	if (ft_strncmp(argv[1], HERE_DOC_STR, ft_strlen(HERE_DOC_STR)) \
-															== SAME_STRING)
-		return (true);
-	return (false);
-}
-
 static void	validate_argc(t_pipex *pipex, int argc)
 {
 	if (argc >= (int)pipex->normal_argc)
@@ -64,15 +69,10 @@ static void	validate_argc(t_pipex *pipex, int argc)
 	}
 }
 
-static void	init_struct(t_pipex *pipex, char **argv)
+static bool	is_here_doc(char **argv)
 {
-	pipex->argv = argv;
-	pipex->pipe_list.head = NULL;
-	pipex->pipe_list.tail = NULL;
-	pipex->pid_list.head = NULL;
-	pipex->pid_list.tail = NULL;
-	pipex->v_argv.list_absolute_path_of_command = NULL;
-	pipex->v_argv.list_of_directory = NULL;
-	pipex->v_argv.temp_divided_list = NULL;
-	pipex->v_argv.virtual_argv = NULL;
+	if (ft_strncmp(argv[1], HERE_DOC_STR, ft_strlen(HERE_DOC_STR)) \
+															== SAME_STRING)
+		return (true);
+	return (false);
 }
