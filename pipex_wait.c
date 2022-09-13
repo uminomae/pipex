846dca08ec2622b99e6ef58873d40e9b9fe870c0
hioikawa <uminomae@gmail.com>
2022-09-13 12:00:08 +0900

    test comment削除

--- pipex_wait.c
+++ pipex_wait.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 00:51:27 by hioikawa          #+#    #+#             */
-/*   Updated: 2022/09/13 04:35:47 by user42           ###   ########.fr       */
+/*   Updated: 2022/09/13 04:58:30 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,11 @@
 void	wait_all_child(t_pipex *pipex)
 {
 	t_pid_node	*node;
-	size_t i=0;
 
 	node = pipex->pid_list.head;
 	while (node != NULL)
 	{
 		x_waitpid(pipex, node->process_id);
 		node = node->next;
-		i++;
-		printf("i=%zu\n", i);
 	}
 }
